f39ba92af38976c81156f8abc8169aacd0b09476
WochiB70 <f1322919711@163.com>
2025-10-20 22:19:45 +0800

    feat(web):增加web模块的认证功能

---
+++ domain-web/src/main/java/xyz/wochib70/web/AuthorizedThreadLocal.java
@@ -0,0 +1,35 @@
+package xyz.wochib70.web;
+
+import xyz.wochib70.domain.UserId;
+
+import java.util.Optional;
+
+public class AuthorizedThreadLocal {
+
+    private static final ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    public static void set(Long userId) {
+        threadLocal.set(userId);
+    }
+
+    public static UserId getAdminId() {
+        if (getOrThrow() > 100000L && getOrThrow() < 200000L) {
+            return new UserId(getOrThrow());
+        }
+        throw new RuntimeException("用户权限不足");
+    }
+
+    public static UserId getUserId() {
+        return new UserId(getOrThrow());
+    }
+
+    public static Long getOrThrow() {
+        return Optional.ofNullable(threadLocal.get())
+                .orElseThrow(() -> new RuntimeException("未授权访问"));
+    }
+
+
+    public static void clean() {
+        threadLocal.remove();
+    }
+}
---
+++ domain-web/src/main/java/xyz/wochib70/web/UserRoleInterceptor.java
@@ -0,0 +1,25 @@
+package xyz.wochib70.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class UserRoleInterceptor implements HandlerInterceptor {
+
+    public static final String USER_ID_HEADER_NAME = "User-Id";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userId = request.getHeader("USER_ID_HEADER_NAME");
+        AuthorizedThreadLocal.set(Long.parseLong(userId));
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        AuthorizedThreadLocal.clean();
+    }
+}
