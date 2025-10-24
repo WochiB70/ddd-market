d0882f68b72b1becace2d3bb51a66e27fb7bbdba
WochiB70 <f1322919711@163.com>
2025-10-24 17:13:48 +0800

    fix(web):修改添加redeemItem的请求参数

--- domain-web/src/main/java/xyz/wochib70/web/mutation/redeem/AddRedeemItemRequest.java
+++ domain-web/src/main/java/xyz/wochib70/web/mutation/redeem/AddRedeemItemRequest.java
@@ -14,34 +14,57 @@ public class AddRedeemItemRequest {
     @NotNull
     @Schema(description = "兑换ID", example = "1")
     private Long redeemId;
-    
+
     @NotNull
     @Schema(description = "商品名称", example = "iPhone 15")
     private String name;
-    
+
     @Schema(description = "商品描述", example = "最新款iPhone手机")
     private String description;
-    
+
     @NotNull
     @Schema(description = "商品类型", example = "PHYSICAL")
     private RedeemItemType type;
-    
+
     @NotNull
-    @Schema(description = "货币ID", example = "1")
-    private Long currencyId;
-    
+    @Schema(description = "兑换项价格")
+    private PriceRequest price;
+
     @NotNull
-    @Schema(description = "商品价格", example = "1000")
-    private Integer price;
-    
-    @Schema(description = "有效数量", example = "100")
-    private Integer validCount;
+    @Schema(description = "兑换项库存")
+    private InventoryRequest inventory;
+
+    public record PriceRequest(
+
+            @NotNull
+            @Schema(description = "货币ID", example = "1")
+            Long currencyId,
+
+            @NotNull
+            @Schema(description = "商品价格", example = "1000")
+            Integer price
+    ) {
+    }
+
+    private record InventoryRequest(
+
+            @NotNull
+            @Schema(description = "库存类型", example = "LIMITED")
+            RedeemItemInventoryType type,
+
+            @Schema(description = "库存数量", example = "100")
+            Integer validCount
+    ) {
+    }
 
     public AddRedeemItemCmd toCmd() {
-        RedeemItemPrice redeemItemPrice = new RedeemItemPrice(new DefaultIdentifierId<>(currencyId), price);
+        RedeemItemPrice redeemItemPrice = new RedeemItemPrice(
+                new DefaultIdentifierId<>(getPrice().currencyId()),
+                getPrice().price()
+        );
         RedeemItemInventory redeemItemInventory = new RedeemItemInventory(
-            validCount != null ? RedeemItemInventoryType.LIMITED : RedeemItemInventoryType.INFINITE,
-            validCount
+                getInventory().type(),
+                getInventory().validCount()
         );
         RedeemItemInfo info = new RedeemItemInfo(name, description, type, redeemItemPrice, redeemItemInventory);
         return new AddRedeemItemCmd(new DefaultIdentifierId<>(redeemId), info);
